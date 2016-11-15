68a825033f488a9311e9ce2d4217571a00f90fb9
Damodar Dahal <underscoredam@gmail.com>
2016-11-15 10:08:49 -0600

    Added energy processor

---
+++ calculate_energy.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, division
+
+with open("energy-used.tmp") as f:
+    file_contents = f.read()
+
+
+values = [0 if not x else float(x.replace("\n", "")) for x in file_contents.split("\n")][0:3]
+
+final_payload_mass = 1000
+mass_loss_on_mars_space = 0.1
+mass_loss_on_lower_orbit_ratio = 0.1
+fuel_percentage = 0.95
+
+initial_payload_mass = ((final_payload_mass / mass_loss_on_mars_space) / mass_loss_on_lower_orbit_ratio)
+fuel_mass = initial_payload_mass * fuel_percentage
+
+first_energy_total = initial_payload_mass * values[0]
+second_energy_total = initial_payload_mass * mass_loss_on_lower_orbit_ratio * values[1]
+third_energy_total = initial_payload_mass * mass_loss_on_lower_orbit_ratio * mass_loss_on_mars_space * values[2]
+
+total_energy = first_energy_total + second_energy_total + third_energy_total
+
+print("Total energy for a payload of {0}kg: {1:1.2e}Joules".format(final_payload_mass, total_energy))
+print("Total fuel carried: {}".format(fuel_mass))
+
+print("Energy provided per kg by fuel: {0:.2e}".format(total_energy/fuel_mass))
+
